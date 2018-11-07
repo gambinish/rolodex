293587e05fe221cd9b126a7f493b7b500aa568ec
Nicholas Gambino <nicholasgambino@Nicholass-MBP.home>
2018-11-06 16:20:33 -1000

    need to wire up post request to add contact

--- src/app/Pages/create-contact/create-contact.component.ts
+++ src/app/Pages/create-contact/create-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../Services/backend.service';
 
 @Component({
   selector: 'create-contact',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateContactComponent implements OnInit {
 
-  constructor() { }
+  formData: {
+    name: string,
+    email: string
+  } = {
+      name: '',
+      email: ''
+    }
+
+  characters: any[] = [];
+
+  render: any;
+
+  sanityCheck: string = 'Sanity Check'
+
+  constructor(private backend: BackendService) { }
 
   ngOnInit() {
+    // simulate database call to retrieve all users smoke test
+    this.characters = this.backend.characters;
+
+    this.backend.getCharacter()
+      .then((data) => {
+        // console.log('data:', data)
+        this.render = data;
+        console.log('this.render', this.render)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+  }
+
+  addContact() {
+    console.log('createContact')
+    console.log('this.formData', this.formData)
+    this.backend.getCharacter()
+      .then((data => {
+        console.log('addContact()', data);
+      }))
+      .catch((err) => {
+        console.log(err);
+      })
+
   }
 
 }
